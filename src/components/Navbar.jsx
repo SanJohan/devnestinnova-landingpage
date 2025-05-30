cdb669578a56d8ae4fa87b2264020eed507cb238
Johan Vargas <johansantiago56@gmail.com>
2025-05-30 18:08:00 -0500

    addimg logo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,8 +15,13 @@ const Navbar = () => {
       <nav className='nav sticky top-0 z-50 py-3 backdrop-blur-lg bg-secondary-background border-b border-navbar-border shadow-lg'>
         <div className='container px-4 mx-auto relative text-sm'>
           <div className='flex justify-between items-center px-18'>
-            <div className=''>
-              <span className='text-xl tracking-tight'>Devnest Innova</span>
+            <div className='flex items-center flex-shrink-0'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-8 w-8 md:h-9 md:w-9 text-nav-anchor-hover mr-2">
+                <path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z"></path>
+                <line x1="16" x2="2" y1="8" y2="22"></line>
+                <line x1="17.5" x2="9" y1="15" y2="15"></line>
+              </svg>
+              <span className='text-2xl tracking-tight font-bold bg-gradient-to-r from-nav-anchor-hover to-secondary bg-clip-text text-transparent'>Devnest Innova</span>
             </div>
             <ul className='hidden lg:flex ml-14 space-x-6'>
               {navItems.map((item,index) => (
@@ -25,8 +30,8 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
-            <div className='hidden lg:flex  justify-center items-center'>
-              <a href="#" className='py-2 px-3 border rounded-md'>Cotizar proyecto</a>
+            <div className='hidden lg:flex  justify-center items-center bg-nav-anchor-hover/10 hover:bg-nav-anchor-hover/20 transition-colors'>
+              <a href="#" className='py-2 px-3 border rounded-md text-nav-anchor-hover'>Cotizar Proyecto</a>
             </div>
             <div className='lg:hidden md:flex flex-col justify-end'>
               <button onClick={toggleNavbar}>
