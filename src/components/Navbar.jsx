cfad7b74ffe1c8f85da9c436406cc766e08e87f5
Johan Vargas <johansantiago56@gmail.com>
2025-06-03 16:30:07 -0500

    navbar in mobile view fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return(
       <nav className='nav sticky top-0 z-50 py-3 backdrop-blur-lg bg-blue-background border-b border-navbar-border shadow-lg'>
         <div className='container px-4 mx-auto relative text-sm'>
-          <div className='flex justify-between items-center px-18'>
+          <div className='flex justify-between items-center md:px-18'>
             <div className='flex items-center flex-shrink-0'>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-8 w-8 md:h-9 md:w-9 text-primary mr-2">
                 <path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z"></path>
@@ -40,7 +40,7 @@ const Navbar = () => {
             </div>
           </div>
           {mobileDrawerOpen && (
-            <div className='fixed right-0 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden'>
+            <div className='fixed right-0 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden text-primary'>
               <ul>
                 {navItems.map((item, index) => (
                   <li key ={index} className='py-4'>
@@ -48,7 +48,8 @@ const Navbar = () => {
                   </li>
                 ))}
               </ul>
-              <a href="#">
+              <a href="#contact" className='text-primary-foreground bg-gradient-to-r from-primary to-secondary py-3 px-4 mx-3 rounded-md hover:opacity-90 hover:scale-105
+            transition-transform duration-300 text-center'>
                 Cotizar un proyecto
               </a>
             </div>
