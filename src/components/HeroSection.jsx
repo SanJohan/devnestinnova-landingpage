0b5a6e2f9003b75d40b88a61132ec9140f3290df
Johan Vargas <johansantiago56@gmail.com>
2025-06-03 17:16:28 -0500

    adding logo to heroection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,15 +2,17 @@ const HeroSection = () => {
   return (
     <section className=" mx-auto px-6 min-h-[calc(100vh-3rem)] md:min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center pt-6 lg:pt-8 bg-gradient-to-br from-background via-slate-900 to-purple-900/30 ">
       <div className="mb-5">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-20 w-20 md:h-28 md:w-28 text-primary stroke-[1.5]"><path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z"></path><line x1="16" x2="2" y1="8" y2="22"></line><line x1="17.5" x2="9" y1="15" y2="15"></line>
-        </svg>
+        <img width={180} height={180} src="./src/assets/devnestlogo.png" alt="" />
+        {/* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-20 w-20 md:h-28 md:w-28 text-primary stroke-[1.5]"><path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z"></path><line x1="16" x2="2" y1="8" y2="22"></line><line x1="17.5" x2="9" y1="15" y2="15"></line>
+        </svg> */}
       </div>
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide 
         bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text font-bold">
         Devnest Innova
       </h1>
       <p className="mt-10 text-lg text-center text-muted-foreground max-w-4xl md:text-xl">
-        Transformamos ideas en soluciones tecnológicas innovadoras. Aplicaciones, software a medida y webs que impulsan tu negocio!
+        Transformamos ideas en soluciones tecnológicas innovadoras.
+        Aplicaciones, software a medida y webs que impulsan tu negocio!
       </p>
       <div className="flex flex-col gap-y-5 justify-center my-10 sm:flex-row">
         <a
