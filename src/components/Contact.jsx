6eb967d95adaf7f89e7f975eaccdb89f2f4d9dac
Johan Vargas <johansantiago56@gmail.com>
2025-07-18 21:24:51 -0500

    contact animated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,17 @@
-
+import { useScrollAnimation} from "../hooks/useScrollAnimation";
+ 
 const Contact = () => {
+  const { ref, isVisible } = useScrollAnimation(0.2);
+  
   return (
-    <section id="contact" className="py-16 md:py-24 bg-bacgkround ">
+    <section ref={ref} id="contact" className="py-16 md:py-24 bg-bacgkround ">
       <div className="container mx-auto px-4 md:px-20">
         <div className="max-w-2xl mx-auto">
-          <h2 className="text-3xl md:text-4xl font-bold text-center mb-2">Hablemos de <span className="gradient-text">Tu Proyecto</span></h2>
-          <p className="text-muted-foreground text-center mb-10">Completa el formulario o contáctanos por nuestras redes. ¡Estamos listos para ayudarte!</p>
+          <div className={`${isVisible ? 'animate-fade-in-up animate-delay-200' : ''}`}>
+            <h2 className="text-3xl md:text-4xl font-bold text-center mb-2">Hablemos de <span className="gradient-text">Tu Proyecto</span></h2>
+            <p className="text-muted-foreground text-center mb-10">Completa el formulario o contáctanos por nuestras redes. ¡Estamos listos para ayudarte!</p>
+          </div>
+          
           <form className="space-y-6 glassmorphism p-8 rounded-xl shadow-2xl">
             <div>
               <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-accent-foreground" for="name">
